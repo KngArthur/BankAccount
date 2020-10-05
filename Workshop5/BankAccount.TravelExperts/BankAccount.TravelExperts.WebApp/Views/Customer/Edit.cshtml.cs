81933abde18b588098cd3fa6807c9f327e39aaf1
Branden Arthur <branden.arthur13@gmail.com>
2020-10-05 10:15:40 -0600

    project start commit

---
+++ Workshop5/BankAccount.TravelExperts/BankAccount.TravelExperts.WebApp/Views/Customer/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BankAccount.TravelExperts.Data.Domain;
+
+namespace BankAccount.TravelExperts.WebApp.Views.Customer
+{
+    public class EditModel : PageModel
+    {
+        private readonly BankAccount.TravelExperts.Data.Domain.TravelExpertsContext _context;
+
+        public EditModel(BankAccount.TravelExperts.Data.Domain.TravelExpertsContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Customers Customers { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Customers = await _context.Customers
+                .Include(c => c.Agent).FirstOrDefaultAsync(m => m.CustomerId == id);
+
+            if (Customers == null)
+            {
+                return NotFound();
+            }
+           ViewData["AgentId"] = new SelectList(_context.Agents, "AgentId", "AgentId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Customers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomersExists(Customers.CustomerId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CustomersExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
+}
---
+++ Workshop5/BankAccount.TravelExperts/BankAccount.TravelExperts.WebApp/Views/Customer/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BankAccount.TravelExperts.Data.Domain;
+
+namespace BankAccount.TravelExperts.WebApp.Views.Customer
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BankAccount.TravelExperts.Data.Domain.TravelExpertsContext _context;
+
+        public IndexModel(BankAccount.TravelExperts.Data.Domain.TravelExpertsContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Customers> Customers { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = await _context.Customers
+                .Include(c => c.Agent).ToListAsync();
+        }
+    }
+}
